683359a9450e32074ef6f5ff62e3969f8d7ab231
k_kwji <87593679+k-kwj@users.noreply.github.com>
2021-11-19 11:31:44 +0900

    Create sample.php

---
+++ sample.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>
+      <p>ここに現れます</p>
+    </h1>
+    <h2>
+    <?php echo $_POST["confirm"]; ?>
+  </h2>
+
+  </body>
+</html>
